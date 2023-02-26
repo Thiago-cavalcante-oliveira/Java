2ff8138972e6140a71b8eae9a0b5fbf20ed5a5c7
Thiago Cavalcante <thhyagofoz@gmail.com>
2023-02-26 10:51:46 -0300

    comitando

---
+++ treino_java/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+
+    ArrayList<String> alunos= new ArrayList<String>();
+
+    public static void main(String[] args) {
+
+        int soma =0;
+        for (int i =0; i <= 1000; i++) {
+            soma +=i;
+        }
+        System.out.println(soma);
+    }
+}
