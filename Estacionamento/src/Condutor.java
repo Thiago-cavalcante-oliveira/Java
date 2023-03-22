40e8e4235e014ad1639564fe7f7f0e039ec327dc
Thiago-cavalcante-oliveira <thhyagofoz@gmail.com>
2023-03-22 16:22:13 -0300

    teste

--- Estacionamento/src/Condutor.java
+++ Estacionamento/src/Condutor.java
@@ -7,8 +7,51 @@ public class Condutor extends Entitty{
     private LocalTime tempoPago;
     private LocalTime tempoDesconto;
 
-    public Condutor (String nome
+    public Condutor(String nome, String cpf, String telefone, LocalTime tempoPago, LocalTime tempoDesconto) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.tempoPago = tempoPago;
+        this.tempoDesconto = tempoDesconto;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalTime getTempoPago() {
+        return tempoPago;
+    }
+
+    public void setTempoPago(LocalTime tempoPago) {
+        this.tempoPago = tempoPago;
+    }
+
+    public LocalTime getTempoDesconto() {
+        return tempoDesconto;
+    }
+
+    public void setTempoDesconto(LocalTime tempoDesconto) {
+        this.tempoDesconto = tempoDesconto;
+    }
 }
--- Estacionamento/src/Main.java
+++ Estacionamento/src/Main.java
@@ -1,5 +1,14 @@
+import j
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        Arraylist<Conduto> = new Condutor();
+
+        condutor
+
     }
 }
